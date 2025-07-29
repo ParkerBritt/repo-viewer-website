71a1cdcda07031bd4a07db6445e0da2e957d7309
parker <parker@parkerbritt.com>
2025-07-29 04:08:20 +0100

    feat: repo link

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
 import './App.css';
 import Card from './Card.js';
+import { useEffect, useState } from "react";
+import { Octokit } from "octokit";
 
 function App() {
-  return (
-    <div className="App">
-          <section className='card-grid'>
-                <Card title="Project" content="Github project description"/>
-                <Card title="Project" content="Github project description"/>
-                <Card title="Project" content="Github project description"/>
-                <Card title="Project" content="Github project description"/>
-                <Card title="Project" content="Github project description"/>
-          </section>
-    </div>
-  );
+    const [repos, setRepos] = useState([]);
+
+    useEffect(() => 
+    {
+        const octokit = new Octokit({
+          // auth: ''
+        })
+
+        async function loadRepos()
+        {
+            try
+            {
+                const response = await octokit.request('GET /users/parkerbritt/repos', {
+                  headers: {
+                    'X-GitHub-Api-Version': '2022-11-28'
+                  }
+                })
+
+                console.log("Reponse data:", response.data)
+                setRepos(response.data);
+            }
+            catch(err)
+            {
+                    console.error("Error fetching repos:", err);
+
+            }
+        }
+
+        loadRepos();
+    }, []);
+
+
+    const cards = [];
+    for(let i=0; i<repos.length; ++i)
+    {
+        cards.push( <Card title={repos.at(i).name} content={repos.at(i).description} url={repos.at(i).html_url}/>)
+    }
+
+    return (
+        <div className="App">
+              <section className='card-grid'>
+                    {cards}
+              </section>
+        </div>
+    );
 }
 
 export default App;
--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import './Card.css';
 
-function Card({title, content})
+function Card({title, content, url})
 {
     return (
-        <div className='card'>
-            <h3>{title}</h3>
-            <p>{content}</p>
-        </div>
+        <a href={url} className="card-link"> <div className='card'>
+            <h3 className="card-title">{title}</h3>
+            <p className="card-description">{content}</p>
+        </div></a>
     );
 
 }
